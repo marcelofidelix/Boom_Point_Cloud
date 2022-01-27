111dd2266e5d00caeae640c8b9aee625f20147c2
Marcelo Fidelix <marcelofidelix@gmail.clom>
2022-01-27 16:13:26 -0300

    identifies boom chord

--- gera_script_salome.py
+++ gera_script_salome.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import pandas as pd
 import pdb
+
 p1 = '''
 #!/usr/bin/env python
 ###
@@ -52,19 +54,44 @@ l = [el for el in l if el != '']
 #Elimina duplicidades nos pontos
 l = list(dict.fromkeys(l))
 
+df = pd.DataFrame({'coords':l})
+df = df['coords'].str.split(',', expand=True)
+for col in df.columns.tolist():
+	df[col] = pd.to_numeric(df[col])
+
+def corda(v1, v2):
+	if v1 > 0 and v2 > 0:
+		return 'SD'
+	if v1 > 0 and v2 < 0:
+		return 'SE'
+	if v1 < 0  and v2 > 0:
+		return 'ID'
+	else:
+		return 'IE'
+
+df['corda'] = df.apply(lambda x: corda(x[1], x[2]), axis=1)
+
+
 #pdb.set_trace()
 
 for i in range(len(l)):
 	p2 += 'Vertex_'+str(i)+' = geompy.MakeVertex('+l[i].replace(' ', ',')+')'+'\n'
 
 for i in range(len(l)):
-	p4 += 'geompy.addToStudy('+ ' Vertex_' + str(i) + ", 'Vertex_" + str(i) + "' )" + '\n'
+	s = df.iloc[i]['corda']
+	p4 += 'geompy.addToStudy('+ ' Vertex_' + str(i) + ", " + "'" + s+str(i)+"' )" + '\n'
 
 for i in range(len(l)-1):
-	p5 += 'Line_' + str(i) + ' = geompy.MakeLineTwoPnt('+ ' Vertex_' + str(i) + ',' + ' Vertex_' + str(i+1) + ")" + '\n'
+	s1 = df.iloc[i]['corda']
+	s2 = df.iloc[i+1]['corda']
+	if s1 == s2:
+		p5 += 'Line_' + str(i) + ' = geompy.MakeLineTwoPnt('+ ' Vertex_' + str(i) + ',' + ' Vertex_' + str(i+1) + ")" + '\n'
 	
 for i in range(len(l)-1):
-	p5 += 'geompy.addToStudy('+ ' Line_' + str(i) + ", 'Line_" + str(i) + "' )" + '\n'
+	s1 = df.iloc[i]['corda']
+	s2 = df.iloc[i+1]['corda']
+	if s1 == s2:
+		p5 += 'geompy.addToStudy('+ ' Line_' + str(i) + ", 'Line_" + str(i) + "' )" + '\n'
 
 file_content = p1 + p2 + p3 + p4 + p5 + p6
 
